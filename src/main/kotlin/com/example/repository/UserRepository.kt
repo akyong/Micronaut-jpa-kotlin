13591f1efbc9b12aec948083a8179357bc383bea
Akyong <ang.kionglam.aries@gmail.com>
2020-06-21 19:59:32 +0700

    add result

--- src/main/kotlin/com/example/repository/UserRepository.kt
+++ src/main/kotlin/com/example/repository/UserRepository.kt
@@ -3,7 +3,6 @@ package com.example.repository
 import com.example.domain.User
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
-import java.util.*
 
 @Repository
 interface UserRepository: CrudRepository<User,Long> {
